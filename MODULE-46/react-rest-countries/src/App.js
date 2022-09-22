c734e24d5d69996857f31b2c9ffa11b63f611242
opuzayed <zayedopu4933@gmail.com>
2022-09-22 10:24:18 +0600

    Test here

--- MODULE-46/react-rest-countries/src/App.js
+++ MODULE-46/react-rest-countries/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello react.How are you</h2>
+        <p>Test you</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
