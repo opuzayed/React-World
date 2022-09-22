5da6b2b9a07c69cd24de877628b8d45085435c0f
opuzayed <zayedopu4933@gmail.com>
2022-09-22 13:35:07 +0600

    add components

--- MODULE-46/react-rest-countries/src/App.js
+++ MODULE-46/react-rest-countries/src/App.js
@@ -1,17 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
-      
+      <Countries></Countries>
     </div>
   );
 }
 
-function LoadCountries(){
+/* function LoadCountries(){
   const [countries, setCountries] = useState([]);
   useEffect(() => {
    fetch('https://restcountries.com/v3.1/all')
@@ -23,7 +22,7 @@ function LoadCountries(){
       <h1>Visiting every countries of the world!!!</h1>
       <h3>Rest Countries : {countries.length}</h3>
       {
-        countries.map(country =><Country name = {country.name.common}></Country>)
+        countries.map(country =><Country name = {country.name.common} population = {country.population}></Country>)
       }
     </div>
   )
@@ -32,8 +31,9 @@ function Country(props){
   return(
       <div>
         <h3>Name : {props.name}</h3>
+        <h4>Population : {props.population}</h4>
       </div>
   )
 }
-
+ */
 export default App;
--- MODULE-46/react-rest-countries/src/components/Countries/Countries.js
+++ MODULE-46/react-rest-countries/src/components/Countries/Countries.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { useState, useEffect } from "react";
+// import Countries from './Countries';
 const Countries = () => {
+    const [countries, setCountries] = useState();
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(data => console.log(data));
+    }, [])
     return (
         <div>
-            <h3>I am from countries.js</h3>
+            <h2>I am from countries</h2>
         </div>
     );
 };
