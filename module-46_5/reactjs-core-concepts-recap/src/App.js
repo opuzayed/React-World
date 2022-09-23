d84f59a6926083870fbb059b98b120ad406deb80
opuzayed <zayedopu4933@gmail.com>
2022-09-23 14:29:33 +0600

    ADD MODULE-46-5 IN REACTJS CORE CONCEPTS-RECAP

---
+++ module-46_5/reactjs-core-concepts-recap/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <District></District>
+    </div>
+  );
+}
+function District(){
+  return(
+    <div>
+      <h2>Name : </h2>
+      <p>Speciality : </p>
+    </div>
+  )
+}
+
+export default App;
