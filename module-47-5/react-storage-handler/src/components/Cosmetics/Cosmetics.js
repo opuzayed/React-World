227b46604bfc71ce1d94b4297570347fbaf59ee1
opuzayed <zayedopu4933@gmail.com>
2022-09-24 15:25:25 +0600

    creat data and show data on ui

--- module-47-5/react-storage-handler/src/components/Cosmetics/Cosmetics.js
+++ module-47-5/react-storage-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Add } from '../../utilities/calculate';
-//import Add from '../../utilities/calculate';
+import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const total = Add(30, 50);
-    return (
+    const cosmetics = [
+        {id : 1, name : 'alta', price : 100},
+        {id : 2, name : 'palish', price : 200},
+        {id : 3, name : 'malish', price : 300},
+        {id : 4, name : 'talish', price : 400},
+        {id : 5, name : 'balish', price : 500}
+    ];
+        return (
+    
         <div>
             <h2>Welcome to my cosmetics store</h2>
-            <p>Total : {total}</p>
+            {
+                cosmetics.map(cosmetic => <Cosmetic 
+                    key = {cosmetic.id}
+                    cosmetic = {cosmetic}
+                    ></Cosmetic>)
+            }
+            
         </div>
     );
 };
