a7039a0cb7fb15270b00aa8f9c278895762166dd
opuzayed <zayedopu4933@gmail.com>
2022-09-24 17:12:45 +0600

    call eventHandler with parameter then first make arrow function then call eventhandler with parameter into arrow function

--- module-47-5/react-storage-handler/src/components/Cosmetic/Cosmetic.js
+++ module-47-5/react-storage-handler/src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import './Cosmetic.css';
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+    const addToCart = (id) =>{
+        console.log('item added', id);
+    }
+    const addToCartWithParam = () => addToCart(id);
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this : {name}</h2>
             <p>Only for : ${price}</p>
             <p><small>ID : {id}</small></p>
+            <button onClick={addToCartWithParam}>Add to Cart</button>
+            <button onClick={() =>addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
