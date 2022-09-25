7f7f4d3b9112b4c170b8b1962e79ed745c9c9ab1
opuzayed <zayedopu4933@gmail.com>
2022-09-25 22:54:32 +0600

    added shopping cart icon into cart button

--- module-48/ema-john-simple/src/components/Product/Product.js
+++ module-48/ema-john-simple/src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 const Product = ({product, handleAddToCart}) => {
     // const {product, handleAddToCart} = props;
@@ -12,7 +14,10 @@ const Product = ({product, handleAddToCart}) => {
                 <p><small>Ratings : {ratings} stars</small></p>
                 <p><small>Seller : {seller}</small></p>
            </div>
-           <button onClick = {()=>handleAddToCart(product)} className='btn-cart'><p>Add to Cart</p></button>
+           <button onClick = {()=>handleAddToCart(product)} className='btn-cart'><p>Add to Cart</p>
+           <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+           </button>
+           
         </div>
     );
 };
