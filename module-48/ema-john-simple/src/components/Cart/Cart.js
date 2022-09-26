0475bf7ffd8b849324a2dc30c3389619bf6868c5
opuzayed <zayedopu4933@gmail.com>
2022-09-27 00:28:08 +0600

    add product partially in local storage

--- module-48/ema-john-simple/src/components/Cart/Cart.js
+++ module-48/ema-john-simple/src/components/Cart/Cart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Cart.css';
 const Cart = ({cart}) => {
-    console.log(cart);
     let total = 0;
     let shipping = 0;
     for (const product of cart)
@@ -9,7 +8,8 @@ const Cart = ({cart}) => {
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
-    let tax = (total * 0.1).toFixed(2);
+    let tax = parseFloat((total * 0.1).toFixed(2));
+    let grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
            <h4>Order Summary</h4>
@@ -17,9 +17,8 @@ const Cart = ({cart}) => {
             <p>Total Price : ${total}</p>
             <p>Total Shipping : ${shipping} </p>
             <p>Tax : {tax}</p> 
-            <h5>Grand Total : </h5>
+            <h5>Grand Total : {grandTotal.toFixed(2)} </h5>
         </div>
     );
 };
-
 export default Cart;
--- module-48/ema-john-simple/src/components/Shop/Shop.js
+++ module-48/ema-john-simple/src/components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css';
 import Cart from './../Cart/Cart';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
@@ -10,9 +11,20 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data));
     }, [])
+
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        for(const id in storedCart)
+        {
+            const addedProduct = products.find(product => product.id === id);
+            console.log(addedProduct);
+        }
+    },[])
+
     const handleAddToCart = (product) =>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
     return (
         <div className='shop-container'>
