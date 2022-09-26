a53a83bb9ad3862812a47d99dca09781cc3e0aef
opuzayed <zayedopu4933@gmail.com>
2022-09-26 22:38:15 +0600

    calculate tax with .toFixed

--- module-48/ema-john-simple/src/components/Cart/Cart.js
+++ module-48/ema-john-simple/src/components/Cart/Cart.js
@@ -9,13 +9,14 @@ const Cart = ({cart}) => {
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
+    let tax = (total * 0.1).toFixed(2);
     return (
         <div className='cart'>
            <h4>Order Summary</h4>
             <h4>Selected Item's : {cart.length}</h4>
             <p>Total Price : ${total}</p>
             <p>Total Shipping : ${shipping} </p>
-            <p>Tax : </p> 
+            <p>Tax : {tax}</p> 
             <h5>Grand Total : </h5>
         </div>
     );
