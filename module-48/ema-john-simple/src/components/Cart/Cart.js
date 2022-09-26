1777a174f667386a9dd5d42461c2f3b62014bb2d
opuzayed <zayedopu4933@gmail.com>
2022-09-26 22:31:25 +0600

    calculate total and shipping cost

--- module-48/ema-john-simple/src/components/Cart/Cart.js
+++ module-48/ema-john-simple/src/components/Cart/Cart.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './Cart.css';
 const Cart = ({cart}) => {
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart)
+    {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
     return (
-        <div>
+        <div className='cart'>
            <h4>Order Summary</h4>
-            <h4>Selected Item's : {cart.length}</h4> 
+            <h4>Selected Item's : {cart.length}</h4>
+            <p>Total Price : ${total}</p>
+            <p>Total Shipping : ${shipping} </p>
+            <p>Tax : </p> 
+            <h5>Grand Total : </h5>
         </div>
     );
 };
--- module-48/ema-john-simple/src/components/Shop/Shop.js
+++ module-48/ema-john-simple/src/components/Shop/Shop.js
@@ -11,7 +11,6 @@ const Shop = () => {
         .then(data => setProducts(data));
     }, [])
     const handleAddToCart = (product) =>{
-        console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
     }
