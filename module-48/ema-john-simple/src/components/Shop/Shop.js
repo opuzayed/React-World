8a16c7c7b63a9cc5172a890732d69c6f55bd179e
opuzayed <zayedopu4933@gmail.com>
2022-09-27 15:36:57 +0600

    cart done

--- module-48/ema-john-simple/src/components/Shop/Shop.js
+++ module-48/ema-john-simple/src/components/Shop/Shop.js
@@ -32,10 +32,23 @@ const Shop = () => {
             }
             setCart(savedCart);
         },[products])
-    const handleAddToCart = (product) =>{
-        const newCart = [...cart, product];
+        const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exist = cart.find(product => product.id === selectedProduct.id);
+        if(!exist)
+        {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else
+        {
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exist.quantity = exist.quantity + 1;
+            newCart = [...rest, exist];
+        }
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className='shop-container'>
