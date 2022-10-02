2adbe83bd4608c39405a3730151e298ff2f80275
opuzayed <zayedopu4933@gmail.com>
2022-10-02 15:56:13 +0600

    create router

---
+++ milestone-9/module-52/explore-routes/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+  const router = createBrowserRouter([
+    {path:'/', element:<div>Default page</div>},
+    {path:'/home', element:<div>Home page</div>},
+    {path:'/about', element:<div>About page</div>}
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
