ab99cbab63e86d212d91578f627e7e10d994cf5b
opuzayed <zayedopu4933@gmail.com>
2022-10-02 22:32:21 +0600

    nested routing

--- milestone-9/module-52/explore-routes/src/components/Home/Home.js
+++ milestone-9/module-52/explore-routes/src/components/Home/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from '../Header/Header';
-
 
 const Home = () => {
     return (
         <div>
-            <Header></Header>
+            
             <h2>This is the real Home World</h2>
         </div>
     );
--- milestone-9/module-52/explore-routes/src/components/Products/Products.js
+++ milestone-9/module-52/explore-routes/src/components/Products/Products.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './../Header/Header';
+
 
 const Products = () => {
     return (
         <div>
-            <Header></Header>
+           
             <h2>This is products</h2>
         </div>
     );
