cc958d51df7d9fa6b52d31718ead18ffb3427cb0
opuzayed <zayedopu4933@gmail.com>
2022-10-03 00:39:24 +0600

    creat dynamic router {}

--- milestone-9/module-52/explore-routes/src/components/Friend/Friend.js
+++ milestone-9/module-52/explore-routes/src/components/Friend/Friend.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import './Friend.css';
+import { Link } from 'react-router-dom';
 const Friend = ({friend}) => {
-    const {name, email} = friend;
+    const {id, name, email, username} = friend;
     return (
-        <div>
+        <div className='friend'>
            <h2> Name : {name}</h2>
            <p>Email : {email}</p>
+           <p><small>UserName :<Link to = {`/friend/${id}`}> {username} </Link></small></p>
         </div>
     );
 };
