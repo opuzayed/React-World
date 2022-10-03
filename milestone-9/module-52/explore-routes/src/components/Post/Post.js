e309101f933f283297af540b1604ed98b53f1a9b
opuzayed <zayedopu4933@gmail.com>
2022-10-03 17:47:38 +0600

    3 ways to navigate

--- milestone-9/module-52/explore-routes/src/components/Post/Post.js
+++ milestone-9/module-52/explore-routes/src/components/Post/Post.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Post.css';
 const Post = ({post}) => {
-    const {title, body} = post;
+    const {id,title, body} = post;
+    const navigate = useNavigate();
+    const handleNavigate = () =>{
+       navigate(`/post/${id}`); 
+    }
     return (
         <div className='post'>
             <p>{title}</p>
             <p><small>{body}</small></p>
-            <button>Show Details</button>
+            <Link to={`/post/${id}`}><button>Show Details</button></Link>
+            <button onClick={handleNavigate}>Show Details2</button>
         </div>
     );
 };
