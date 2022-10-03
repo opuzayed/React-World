b2844a7f3f71d4fc3972ad697850f09962667774
opuzayed <zayedopu4933@gmail.com>
2022-10-03 15:42:54 +0600

    use loader for data loaded

---
+++ milestone-9/module-52/explore-routes/src/components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Posts = () => {
+    return (
+        <div>
+           <h2>Lot's of fb post are here</h2> 
+        </div>
+    );
+};
+
+export default Posts;
