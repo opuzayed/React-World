a68b05f3b181bbd5c26c17c84610d478df7c8c74
opuzayed <zayedopu4933@gmail.com>
2022-10-05 17:10:16 +0600

    country loaded

---
+++ milestone-9/module-52/routed-countries/src/routes/routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Countries from "../components/Countries";
+
+export const router  = createBrowserRouter([
+    {path:'/',
+        loader:async() =>{
+            return fetch('https://restcountries.com/v3.1/all');
+        },
+     element:<Countries></Countries>}
+]);
