97482e813877e992971f9a45d682051a85a8a938
opuzayed <zayedopu4933@gmail.com>
2022-10-06 22:41:36 +0600

    install router dom

---
+++ milestone-9/module-52/again-router/src/App.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  const router = createBrowserRouter([
+
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
