5bfcd4e2ff1bf905024b0af33d04a0bff6e44c06
opuzayed <zayedopu4933@gmail.com>
2022-10-09 14:24:59 +0600

    set router and make lot of components

---
+++ milestone-9/module-54/tshirts-mania/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
+import Orders from './components/Orders/Orders';
+
+function App() {
+  const router = createBrowserRouter([
+   { path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'orders',
+        element:<Orders></Orders>
+      }
+    ]
+  }
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
