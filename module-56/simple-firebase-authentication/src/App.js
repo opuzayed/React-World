9bc04ca5df45b4eac78f9a6e0fc80bd84ef2eaa2
opuzayed <zayedopu4933@gmail.com>
2022-10-14 01:37:23 +0600

    sign in

--- module-56/simple-firebase-authentication/src/App.js
+++ module-56/simple-firebase-authentication/src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase/firebase.init';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
-    .error(error =>{
-      console.error('error:', error);
+    .error(error => {
+        console.log('error : ', error);
     })
   }
+    const handleSignOut = () =>{
+      signOut(auth)
+      .then(() =>{
+        setUser({});
+      })
+      .error(error =>{
+        setUser({});
+      })
+    }
+  
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+     <button onClick={handleGoogleSignIn}>Google Sign In</button>
+     <button onClick={handleSignOut}>Sign Out</button>
+     <div>
+          DisplayName : {user.displayName} 
+          <p>Email :{user.email}</p>
+          
+     </div>
     </div>
   );
 }
