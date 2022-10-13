facf6366299733b4472fc0d8b84af228b08e1a84
opuzayed <zayedopu4933@gmail.com>
2022-10-13 22:53:19 +0600

    install

---
+++ module-56/simple-firebase-authentication/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import app from './firebase/firebase.init';
+
+const auth = getAuth(app);
+function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    console.log('google comming');
+  }
+  return (
+    <div className="App">
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ module-56/simple-firebase-authentication/src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiPtxHISlhkR9cJ19EEelPUPwkO9Pzo8w",
+  authDomain: "simple-firebase-authenti-6d891.firebaseapp.com",
+  projectId: "simple-firebase-authenti-6d891",
+  storageBucket: "simple-firebase-authenti-6d891.appspot.com",
+  messagingSenderId: "398515311415",
+  appId: "1:398515311415:web:42014493662aa496c140b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
