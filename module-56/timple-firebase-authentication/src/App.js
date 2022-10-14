97c4d8c97a264357ea26c78c3e10e9ac4e94856e
opuzayed <zayedopu4933@gmail.com>
2022-10-14 18:34:35 +0600

    auth

---
+++ module-56/timple-firebase-authentication/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import app from './firebase/firebase.init';
+const auth = getAuth(app);
+function App() {
+  const googleProvider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+  }
+  return (
+    <div className="App">
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ module-56/timple-firebase-authentication/src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuom8zw-v3we3VzKdaocQor_2VdOu5t2w",
+  authDomain: "timple-firebase-project-b942a.firebaseapp.com",
+  projectId: "timple-firebase-project-b942a",
+  storageBucket: "timple-firebase-project-b942a.appspot.com",
+  messagingSenderId: "804209476115",
+  appId: "1:804209476115:web:a9cd2ec2387db32f5035e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
