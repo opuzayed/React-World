61fb462265c0ba218bd1806c58d9b2dd7fbfb729
opuzayed <zayedopu4933@gmail.com>
2022-10-15 22:12:20 +0600

    register added

--- module-57/email-password-auth/src/components/RegisterReactBootstrap.js
+++ module-57/email-password-auth/src/components/RegisterReactBootstrap.js
@@ -9,20 +9,28 @@ const RegisterReactBootstrap = () => {
         const password = event.target.password.value;
         console.log(email, password);
     }
+    const handleRegisterBlur = event => {
+        const email = event.target.value;
+        console.log(email);
+    }
+    const handlePasswordBlur = event => {
+        const password = event.target.value;
+        console.log(password);
+    }
     return (
         <div className='w-50 mx-auto'>
             <h3 className='text-warning'>Please Register</h3>
             <Form onSubmit={handleRegister}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter email" />
+        <Form.Control onBlur={handleRegisterBlur} type="email" name="email" placeholder="Enter email" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name='password' placeholder="Password" />
+        <Form.Control onBlur={handlePasswordBlur}type="password" name='password' placeholder="Password" />
       </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        Register
       </Button>
     </Form>
         </div>
