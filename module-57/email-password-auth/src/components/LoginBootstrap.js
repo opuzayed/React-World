525139e72b9c8316a7a06acfce9a4db5b9eac27c
opuzayed <zayedopu4933@gmail.com>
2022-10-16 14:18:05 +0600

    LOGIN FILE DONE

--- module-57/email-password-auth/src/components/LoginBootstrap.js
+++ module-57/email-password-auth/src/components/LoginBootstrap.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const LoginBootstrap = () => {
-    return (
-        <div>
-            <h3>Please Log In!!!!</h3>
-        </div>
-    );
+  return (
+    <div className='w-50 mx-auto'>
+      <h3 className='text-success'>Please Log In!!!!</h3>
+      <div class="mb-3">
+        <label for="formGroupExampleInput" class="form-label">
+         Email
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="formGroupExampleInput"
+          placeholder="Enter a valid email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Enter a password"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default LoginBootstrap;
