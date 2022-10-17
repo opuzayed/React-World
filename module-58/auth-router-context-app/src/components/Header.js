5cf9927b434380741665224edaef32033e1d44e3
opuzayed <zayedopu4933@gmail.com>
2022-10-17 15:37:59 +0600

    login done

---
+++ module-58/auth-router-context-app/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg-primary text-primary-content">
+        <Link to='/' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to='/login'>Log in</Link>
+        <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
