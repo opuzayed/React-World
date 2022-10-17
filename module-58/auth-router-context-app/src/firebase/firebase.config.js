c32b1d77bc090c15616d3f390fff4bfec9a3420e
opuzayed <zayedopu4933@gmail.com>
2022-10-17 22:10:27 +0600

    context

---
+++ module-58/auth-router-context-app/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZZ98ukoZUMtEN9gnO37bsixMv35XfqyE",
+  authDomain: "auth-router-context-9ce8b.firebaseapp.com",
+  projectId: "auth-router-context-9ce8b",
+  storageBucket: "auth-router-context-9ce8b.appspot.com",
+  messagingSenderId: "453683021924",
+  appId: "1:453683021924:web:876e15911a9cd623a9f771"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
