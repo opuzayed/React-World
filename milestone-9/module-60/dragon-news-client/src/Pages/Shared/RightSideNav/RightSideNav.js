9aa8e77640ca8278febd04e2a3266aee3bb1b1bd
opuzayed <zayedopu4933@gmail.com>
2022-10-20 22:12:55 +0600

    icon added from reacticons site

--- milestone-9/module-60/dragon-news-client/src/Pages/Shared/RightSideNav/RightSideNav.js
+++ milestone-9/module-60/dragon-news-client/src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-
+import { FaGoogle, FaGithub } from "react-icons/fa";
 const RightSideNav = () => {
   return (
     <div>
       <ButtonGroup vertical>
-        <Button variant="outline-success">Login with Google</Button>
-        <Button variant="outline-info">Login with Github</Button>
+        <Button variant="outline-success">Login with Google <FaGoogle></FaGoogle></Button>
+        <Button variant="outline-info">Login with Github <FaGithub></FaGithub></Button>
       </ButtonGroup>
     </div>
   );
