465eba42ab231e09e05acc1425ec0e5df6403605
opuzayed <zayedopu4933@gmail.com>
2022-10-20 23:44:43 +0600

    carousel added

--- milestone-9/module-60/dragon-news-client/src/Pages/Shared/RightSideNav/RightSideNav.js
+++ milestone-9/module-60/dragon-news-client/src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,14 +1,34 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import { FaGoogle, FaGithub } from "react-icons/fa";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaLinkedin, FaMicrosoft, FaTwitch } from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
+
 const RightSideNav = () => {
   return (
     <div>
       <ButtonGroup vertical>
-        <Button variant="outline-success">Login with Google <FaGoogle></FaGoogle></Button>
-        <Button variant="outline-info">Login with Github <FaGithub></FaGithub></Button>
+        <Button className="mb-2" variant="outline-success">
+          Login with Google <FaGoogle></FaGoogle>
+        </Button>
+        <Button variant="outline-info">
+          Login with Github <FaGithub></FaGithub>
+        </Button>
       </ButtonGroup>
+      <div className='mt-3'>
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+          <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+          <ListGroup.Item className='mb-2'><FaLinkedin></FaLinkedin>Linkedin</ListGroup.Item>
+          <ListGroup.Item className='mb-2'><FaMicrosoft></FaMicrosoft>Microsoft</ListGroup.Item>
+          <ListGroup.Item className='mb-2'><FaTwitch></FaTwitch>Twitch</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div className='mt-4'>
+        <BrandCarousel></BrandCarousel>
+      </div>
     </div>
   );
 };
