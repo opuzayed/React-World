a4f9615eaaa3d5f7272111c38a2bd5dd6c9bf74e
opuzayed <zayedopu4933@gmail.com>
2022-10-20 14:42:17 +0600

    project set up with folder structure make

---
+++ milestone-9/module-60/dragon-news-client/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw-Mm-yfJDUZSxis8m-WwYkglLnwZYn-U",
+  authDomain: "dragon-news-695ba.firebaseapp.com",
+  projectId: "dragon-news-695ba",
+  storageBucket: "dragon-news-695ba.appspot.com",
+  messagingSenderId: "765536448944",
+  appId: "1:765536448944:web:d42cbab082c7082f0f6675"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ milestone-9/module-60/dragon-news-client/src/layout/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "../Pages/Shared/Header/Header";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Pages/Shared/RightSideNav/RightSideNav";
+
+const Main = () => {
+  return (
+        <div>
+            <Header></Header>
+        <container>
+            <Row>
+            <Col lg='2'>
+                <LeftSideNav></LeftSideNav>
+            </Col>
+            <Col lg='7'>
+                <Outlet></Outlet>
+            </Col>
+            <Col lg='3'>
+                <RightSideNav></RightSideNav>
+            </Col>
+            </Row>
+        </container>
+    </div>
+  );
+};
+
+export default Main;
