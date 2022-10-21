dd318873a927ee75c198112486366de98cbc2592
opuzayed <zayedopu4933@gmail.com>
2022-10-22 00:35:55 +0600

    create context and use context

---
+++ milestone-9/module-60/dragon-news-client/src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+const AuthProvider = ({children}) => {
+    const user = {displayName : 'opu'}
+    const authInfo = {user};
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
